798472d572004b7eaaf7c12f4c58800d0591f0d7
Skralderalle1 <rasmusoj1@live.dk>
2024-08-26 10:46:19 +0200

    update

--- code/rasmus_test.py
+++ code/rasmus_test.py
@@ -1,9 +1,17 @@
 import requests
+import datetime as dt
 
 URL = "https://api.energifyn.dk/api/graph/consumptionprice?date=26-08-2024"
+timenr = (dt.datetime.now().strftime('%H'))
 
-responce = requests.get(URL)
-data = responce.json()
-data_iter = responce.iter_lines()
-print(responce)
-print(data)
+def fetch(h, x):
+    responce =requests.get(URL)
+    data = responce.json()
+    if x == "west":
+        return data["westPrices"]["2024-08-26T00:00:00"]["prices"][h]["price"]
+    elif x == "east":
+        return data["eastPrices"]["2024-08-26T00:00:00"]["prices"][h]["price"]
+        
+print(fetch(int(timenr),"west"))
+print(fetch(int(timenr),"east"))
+
