32384869a623d564337f32c0de0bd3f7fd5668f5
NoahRosenkjaer <noahrosenkjaer@gmail.com>
2024-08-26 15:31:22 +0200

    Små ændringer

--- code/data_fetch_test.py
+++ code/data_fetch_test.py
@@ -1,23 +1,28 @@
+import datetime as dt
 import requests
 import datetime
-import datetime as dt
 
-def get(h: int, compas: str, data: dict) -> dict:
-    dato = datetime.datetime.now().isoformat()
-    dato = dato[0:11] + "00:00:00"
-    if compas == "east":
-        return data['eastPrices'][str(dato)]['prices'][h]
-    else:
-        return data['westPrices'][str(dato)]['prices'][h]
-    
+# Constant values
+dato = datetime.datetime.now().isoformat()
+DATO = dato[0:11] + "00:00:00"
+TIME = (dt.datetime.now().strftime('%H'))
 
-def fetch():
+def fetch() -> dict:
     URL = "https://api.energifyn.dk/api/graph/consumptionprice?date=26-08-2024"
-    timenr = (dt.datetime.now().strftime('%H'))
     response = requests.get(URL)
     data = response.json()
-    print(get(int(timenr), "west", data))
-    print(get(int(timenr), "east", data))
-    print(response)
-    
-fetch()
+
+    return data
+
+def get(h: int, compas: str, data: dict, date: str):
+    if compas == "east":
+        print(data['eastPrices'][str(date)]['prices'][h])
+        return data['eastPrices'][str(date)]['prices'][h]
+    else:
+        print(data['westPrices'][str(date)]['prices'][h])
+        return data['westPrices'][str(date)]['prices'][h]
+
+
+data = fetch()
+get(int(TIME), "west", data, DATO)
+get(int(TIME), "east", data, DATO)
