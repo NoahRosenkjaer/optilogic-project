e880dfd1092f34c91854700ffec0045fb8eb39f1
Noah_Rosenkjaer <ncro58767@edu.ucl.dk>
2024-08-26 14:49:43 +0200

    Testing

--- code/data_fetch_test.py
+++ code/data_fetch_test.py
@@ -1,19 +1,23 @@
 import requests
+import datetime
+import datetime as dt
 
-def get(h, compas, data):
+def get(h: int, compas: str, data: dict) -> dict:
+    dato = datetime.datetime.now().isoformat()
+    dato = dato[0:11] + "00:00:00"
     if compas == "east":
-        return data['eastPrices']['2024-08-26T00:00:00']['prices'][h]
+        return data['eastPrices'][str(dato)]['prices'][h]
     else:
-        return data['westPrices']['2024-08-26T00:00:00']['prices'][h]
-        
+        return data['westPrices'][str(dato)]['prices'][h]
+    
 
 def fetch():
     URL = "https://api.energifyn.dk/api/graph/consumptionprice?date=26-08-2024"
-
+    timenr = (dt.datetime.now().strftime('%H'))
     response = requests.get(URL)
     data = response.json()
-    print(get(10, "west", data))
-    print(get(10, "east", data))
+    print(get(int(timenr), "west", data))
+    print(get(int(timenr), "east", data))
     print(response)
     
 fetch()
