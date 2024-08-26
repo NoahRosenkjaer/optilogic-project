e7ec2157c2bd23de03ccef3d745ec5eee7bde8e7
NoahRosenkjaer <noahrosenkjaer@gmail.com>
2024-08-26 16:17:34 +0200

    added graph

--- code/data_fetch_test.py
+++ code/data_fetch_test.py
@@ -3,8 +3,7 @@ import requests
 import datetime
 
 # Constant values
-dato = datetime.datetime.now().isoformat()
-DATO = dato[0:11] + "00:00:00"
+DATO = datetime.datetime.now().isoformat()[0:11] + "00:00:00"
 TIME = (dt.datetime.now().strftime('%H'))
 
 def fetch() -> dict:
@@ -22,7 +21,25 @@ def get(h: int, compas: str, data: dict, date: str):
         print(data['westPrices'][str(date)]['prices'][h])
         return data['westPrices'][str(date)]['prices'][h]
 
+def graph(data):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    x, y = [], []
+
+    for element in data['westPrices']['2024-08-26T00:00:00']['prices']:
+        x.append(element['hour'][11:13])
+        y.append(element['price'])
+
+    fig, ax = plt.subplots()
+
+    ax.bar(x, y, width=1, edgecolor="white", linewidth=0.5)
+    ax.set_ylabel("Price [kr./kWh]")
+    ax.set_xlabel("Time [Hours]")
+
+    plt.show()
 
 data = fetch()
 get(int(TIME), "west", data, DATO)
 get(int(TIME), "east", data, DATO)
+graph(data)
