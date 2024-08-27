fb5827079cce3b2e71f044592a8091babc902e0b
Skralderalle1 <rasmusoj1@live.dk>
2024-08-27 09:01:37 +0200

    update

--- code/rasmus_test.py
+++ code/rasmus_test.py
@@ -2,19 +2,25 @@ import requests
 import datetime as dt
 import datetime
 
-URL = "https://api.energifyn.dk/api/graph/consumptionprice?date=26-08-2024"
+DATE = dt.datetime.now().strftime("%d-%m-%Y")
+URL = f"https://api.energifyn.dk/api/graph/consumptionprice?date={DATE}"
+
 timenr = (dt.datetime.now().strftime('%H'))
 dato = datetime.datetime.now().isoformat()
 dato1 = dato[0:11] + "00:00:00"
 
 def fetch(h,x):
-    responce =requests.get(URL)
-    data = responce.json()
+    try:
+        responce =requests.get(URL)
+        data = responce.json()
 
-    if x == "west":
-        return data["westPrices"][str(dato1)]["prices"][h]["price"]
-    elif x == "east":
-        return data["eastPrices"][str(dato1)]["prices"][h]["price"]
+        if x == "west":
+            return data["westPrices"][str(dato1)]["prices"][h]["price"]
+        elif x == "east":
+            return data["eastPrices"][str(dato1)]["prices"][h]["price"]
+        
+    except Exception as e:
+        print(e)
         
 print(fetch(int(timenr),"west"))
 print(fetch(int(timenr),"east"))
