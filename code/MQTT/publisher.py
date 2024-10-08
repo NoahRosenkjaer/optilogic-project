a3e5c123958404629677b34ccdc87522ccb24f43
NoahRosenkjaer <noahrosenkjaer@gmail.com>
2024-10-08 11:26:28 +0200

    yes

--- code/MQTT/publisher.py
+++ code/MQTT/publisher.py
@@ -11,6 +11,7 @@ myQoS     = 1
 myRetain  = True
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.username_pw_set("user", "$7$101$yf94T8QG4zvJlcjM$EqNZNhFdEzWYU7WD3b1Eqj7XJ/qk6zSR1aqeYi8mf15o3vjuaBB5tVn8mLKJA3o+cwggfEW2atZm64X+fNGebg==")
 client.connect(brokerIP, brokerPort, brokerKeepAlive)
 client.publish(topic =  myTopic, qos = myQoS, payload = myPayload, retain = myRetain);
 
--- code/MQTT/subscriber.py
+++ code/MQTT/subscriber.py
@@ -4,18 +4,9 @@ import time
 broker = '127.0.0.1'
 topic = 'test'
 
-# def get_info():
-#     with open("C:\\Program files", 'r') as file:
-#         line = file.readline()
-#     print(line)
-
-
 def on_connect(client, userdata, flags, rc, properties):
     print("Connected with result code " + str(rc))
     print(f"Subscriber til {topic}")
-
-def on_publish(client, userdata, mid):
-    print("Data published \n")
 
 
 def on_message(client, userdata, message):
@@ -23,19 +14,16 @@ def on_message(client, userdata, message):
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
+client.username_pw_set("user", "$7$101$yf94T8QG4zvJlcjM$EqNZNhFdEzWYU7WD3b1Eqj7XJ/qk6zSR1aqeYi8mf15o3vjuaBB5tVn8mLKJA3o+cwggfEW2atZm64X+fNGebg==")
+
 print("connecting to broker")
 client.connect(broker, port=1883)
 client.on_connect = on_connect
 client.on_message = on_message
 
-try:
-    client.username_pw_set("admin", "$7$101$yf94T8QG4zvJlcjM$EqNZNhFdEzWYU7WD3b1Eqj7XJ/qk6zSR1aqeYi8mf15o3vjuaBB5tVn8mLKJA3o+cwggfEW2atZm64X+fNGebg==")
-except:
-    print("could not authenticate")
 
 client.loop_start()
 client.subscribe(topic)
 while True:
-    
     client.on_message
     time.sleep(5)
